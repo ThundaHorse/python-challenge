b6ba61bc452b6e5ff11a99816c378ef183320afb
Abraham Kim <abeeeeekim@gmail.com>
2025-02-20 17:37:15 -0500

    cleaning up function, need to refactor

--- report.md
+++ report.md
@@ -1,14 +1,16 @@
-product_name,our_price,category,current_stock,restock_threshold,date
-Organic Coffee Beans (1lb),14.99,Beverages,45,25,15/11/2024
-Premium Green Tea (50 bags),8.99,Beverages,32,20,10/11/2024
-Masala Chai Mix (12oz),9.99,beverages,18,15,18/11/2024
-Yerba Mate Loose Leaf (1lb),$12.99,Beverages,5,10,01/11/2024
-Hot Chocolate Mix (1lb),7.99,Beverages,50,30,
-Earl Grey Tea (100 bags),11.99,beverages,28,25,14/11/2024
-Espresso Beans (1lb),16.99,Beverages,22,20,16/11/2024
-Chamomile Tea (30 bags),6.99,Tea,12,15,05/11/2024
-Matcha Green Tea Powder (4oz),19.99,Beverages,8,,17/11/2024
-Decaf Coffee Beans (1lb),15.99,Beverages,15,15,13/11/2024
-Mint Tea (25 bags),7.49,Beverages,out of stock,12,30/10/2024
-Instant Coffee (8oz),11.99,coffee,25,20,19/11/2024
-cold brew concentrate,13.99,Beverages,19,15,20/11/2024
+| product_name | our_price | category | current_stock | restock_threshold | date |
+| --- | --- | --- | --- | --- | --- |
+| "Organic Coffee Beans (1lb)" | 14.99 | Beverages | 45 | 25 | 11-15-2024 |
+| "Premium Green Tea (50 bags)" | 8.99 | Beverages | 32 | 20 | 11-10-2024 |
+| "Masala Chai Mix (12oz)" | 9.99 | Beverages | 18 | 15 | 11-18-2024 |
+| "Yerba Mate Loose Leaf (1lb)" |  12.99 | Beverages | 5 | 10 | 11-01-2024 |
+| "Hot Chocolate Mix (1lb)" | 7.99 | Beverages | 50 | 30 | 11-12-2024 |
+| "Earl Grey Tea (100 bags)" | 11.99 | Beverages | 28 | 25 | 11-14-2024 |
+| "Espresso Beans (1lb)" | 16.99 | Beverages | 22 | 20 | 11-16-2024 |
+| "Chamomile Tea (30 bags)" | 6.99 | Tea | 12 | 15 | 11-05-2024 |
+| "Matcha Green Tea Powder (4oz)" | 19.99 | Beverages | 8 | 0 | 11-17-2024 |
+| "Decaf Coffee Beans (1lb)" | 15.99 | Beverages | 15 | 15 | 11-13-2024 |
+| "Mint Tea (25 bags)" | 7.49 | Beverages | out of stock | 12 | 10-30-2024 |
+| "Instant Coffee (8oz)" | 11.99 | Coffee | 25 | 20 | 11-19-2024 |
+| "Rooibos Tea (40 bags)" | 0 | Beverages | 30 | 20 | 11-08-2024 |
+| "cold brew concentrate" | 13.99 | Beverages | 19 | 15 | 11-20-2024 |
--- src/analysis.py
+++ src/analysis.py
@@ -1,22 +1,32 @@
 import csv
 import sys
 from datetime import datetime
+import re
 
 
-def copy_csv_data(input_filepath, output_filepath):
+def csv_to_markdown_table(csv_filepath, markdown_filepath):
     """
-    Reads data from a CSV file and writes it to a new CSV file.
+    Convert desired CSV file to Markdown table and save it to a report.md.
 
     Args:
-        input_filepath (str): The path to the input CSV file.
-        output_filepath (str): The path to the output CSV file.
+        csv_filepath (str): The path to the input CSV file.
+        markdown_filepath (str): The path to the output Markdown file.
     """
+
+    # Error Handling
     try:
         with (
-            open(input_filepath, "r") as infile,
-            open(output_filepath, "w", newline="") as outfile,
+            open(csv_filepath, "r") as csvfile,
+            open(markdown_filepath, "w", newline="") as mdfile,
         ):
-            fieldnames = [
+            # Set reader to read from dict
+            # Handle cases where csv is not properly delimited
+            # i.e. Rooibos Tea was not properly delimited
+            reader = csv.DictReader(csvfile, quoting=csv.QUOTE_NONE, escapechar="\\")
+
+            # Extract headers
+            # Set desired information
+            headers = [
                 "product_name",
                 "our_price",
                 "category",
@@ -24,41 +34,86 @@ def copy_csv_data(input_filepath, output_filepath):
                 "restock_threshold",
                 "date",
             ]
-            reader = csv.DictReader(infile)
 
-            writer = csv.DictWriter(
-                outfile, fieldnames=fieldnames, delimiter=",", extrasaction="ignore"
-            )
-            writer.writeheader()
+            # Set initial Row
+            mdfile.write("| " + " | ".join(headers) + " |\n")
+            mdfile.write("| " + " | ".join(["---"] * len(headers)) + " |\n")
 
             for row in reader:
                 cleaned_row = {k: v for k, v in row.items() if k is not None}
 
-                date_string = list(
-                    {k: v for k, v in row.items() if k is None}.values()
-                )[0][0]
+                try:
+                    date_string = list(
+                        {k: v for k, v in row.items() if k is None}.values()
+                    )[0][0]
 
-                if "/" in date_string:
-                    date_string = date_string.replace("/", "-")
-                    date_object = datetime.strptime(date_string, "%m-%d-%Y")
-                    formatted_date_string = date_object.strftime("%d/%m/%Y")
-                else:
-                    date_object = datetime.strptime(date_string, "%Y-%m-%d")
-                    formatted_date_string = date_object.strftime("%d/%m/%Y")
+                    # Clean up formatting for our_price, current_stock, and restock_threshold if no value & remove dollar sign if present
+                    cleaned_row["our_price"] = (
+                        cleaned_row["our_price"].replace("$", " ")
+                        if "$" in cleaned_row["our_price"]
+                        else cleaned_row["our_price"]
+                    )
+                    cleaned_row["our_price"] = (
+                        "0"
+                        if cleaned_row["our_price"] == ""
+                        else cleaned_row["our_price"]
+                    )
+                    cleaned_row["current_stock"] = (
+                        cleaned_row["current_stock"].replace("$", " ")
+                        if "$" in cleaned_row["current_stock"]
+                        else cleaned_row["current_stock"]
+                    )
+                    cleaned_row["current_stock"] = (
+                        "0"
+                        if cleaned_row["current_stock"] == ""
+                        else cleaned_row["current_stock"]
+                    )
+                    cleaned_row["restock_threshold"] = (
+                        cleaned_row["restock_threshold"].replace("$", " ")
+                        if "$" in cleaned_row["restock_threshold"]
+                        else cleaned_row["restock_threshold"]
+                    )
+                    cleaned_row["restock_threshold"] = (
+                        "0"
+                        if cleaned_row["restock_threshold"] == ""
+                        else cleaned_row["restock_threshold"]
+                    )
+
+                    cleaned_row["product_name"] = (
+                        cleaned_row["product_name"].strip('"').capitalize()
+                        if "$" in cleaned_row["product_name"]
+                        else cleaned_row["product_name"]
+                    )
+                    cleaned_row["category"] = cleaned_row["category"].capitalize()
+
+                    # Date format error, manual fix
+                    if "/" in date_string:
+                        date_string = date_string.replace("/", "-")
+                        date_object = datetime.strptime(date_string, "%m-%d-%Y")
+                        formatted_date_string = date_object.strftime("%m-%d-%Y")
+                    else:
+                        date_object = datetime.strptime(date_string, "%Y-%m-%d")
+                        formatted_date_string = date_object.strftime("%m-%d-%Y")
+
                     cleaned_row["date"] = formatted_date_string
-                print(cleaned_row)
-                writer.writerow(cleaned_row)
+                    mdfile.write(
+                        "| " + " | ".join(cleaned_row[h] for h in headers) + " |\n"
+                    )
+                except Exception as e:
+                    print(f"An error occurred: {e}")
 
     except FileNotFoundError:
-        print(f"Error: Input file '{input_filepath}' not found.")
+        print(f"Error: Input file '{csv_filepath}' not found.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred when parsing: {e}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         input_csv_file = sys.argv[1]
         output_file = "report.md"
-        copy_csv_data(input_filepath=input_csv_file, output_filepath=output_file)
+        csv_to_markdown_table(
+            csv_filepath=input_csv_file, markdown_filepath=output_file
+        )
     else:
         print("Please provide an input csv file.")
